b2ef5b19c50d72625025bba6d2de9e905fb7fd07
Si4tsu <gazda.staszek@gmail.com>
2024-12-01 15:43:04 +0100

    Day 1

--- Day1.py
+++ Day1.py
@@ -1,27 +1,5 @@
 from sys import stdin
 
-#Test input
-'''
-3   4
-4   3
-2   5
-1   3
-3   9
-3   3
-'''
-
-#Input to lists
-lines = stdin.readlines()
-
-list1: list = []
-list2: list = []
-
-for line in lines:
-    line = line.split("   ")
-
-    list1.append(int(line[0]))
-    list2.append(int(line[1]))
-
 #Calculate distance
 def get_distance(list1, list2):
     list1.sort()
@@ -33,5 +11,19 @@ def get_distance(list1, list2):
         res += abs(list1[i] - list2[i])
     
     return res
+
+#Input to lists
+with open("Day1.in") as file:
+
+    lines = file.readlines()
+
+    list1: list = []
+    list2: list = []
+
+    for line in lines:
+        line = line.split("   ")
+
+        list1.append(int(line[0]))
+        list2.append(int(line[1]))
 
 print(get_distance(list1, list2))
