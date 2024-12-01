3a29f0ccdfd8a2131d9de6eae63f198f522c1327
Si4tsu <gazda.staszek@gmail.com>
2024-12-01 15:47:17 +0100

    Day 1 fix

--- Day1_2.py
+++ Day1_2.py
@@ -1,27 +1,5 @@
 from sys import stdin
 
-#Test input
-'''
-3   4
-4   3
-2   5
-1   3
-3   9
-3   3
-'''
-
-#Input to lists
-lines = stdin.readlines()
-
-list1: list = []
-list2: list = []
-
-for line in lines:
-    line = line.split("   ")
-
-    list1.append(int(line[0]))
-    list2.append(int(line[1]))
-
 #Calculate similarity
 def get_similarity(list1, list2):
     
@@ -31,5 +9,18 @@ def get_similarity(list1, list2):
         res += id * list2.count(id)
     
     return res
+
+#Input to lists
+with open("Day1.in") as file:
+    lines = file.readlines()
+
+    list1: list = []
+    list2: list = []
+
+    for line in lines:
+        line = line.split("   ")
+
+        list1.append(int(line[0]))
+        list2.append(int(line[1]))
 
 print(get_similarity(list1, list2))
